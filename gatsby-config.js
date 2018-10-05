2960a6020820594fe1231b380dbc33da7ba30102
jai <jaikant@gmail.com>
2018-10-05 10:30:12 +0530

    Removed the offline and manifest packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,6 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-      },
-    },
     {
       resolve : `gatsby-source-filesystem`,
       options: {
@@ -25,6 +11,5 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-offline',
   ],
 }
