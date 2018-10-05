6050a7e616c9299914d1829841c64e882447c9b4
jai <jaikant@gmail.com>
2018-10-05 10:26:17 +0530

    Simplifying further

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import { graphql } from 'gatsby'
 
-import Layout from '../components/layout'
+export default () => <div> Hello! </div>
 
-const IndexPage = (props) => {
-  const { edges } = props.data.allMarkdownRemark
-  return (
-  <Layout>
-    {
-      edges.map((md, index) => <div key={index}>
-        <p> { md.node.frontmatter.title } </p>
-      </div>)
-    }
-  </Layout>
-)
-  }
-
-export default IndexPage
-
-export const query = graphql`
-query {
-  allMarkdownRemark {
-    edges {
-      node {
-        frontmatter {
-          title
-        }
-        html
-      }
-    }
-  }
-}
-`
