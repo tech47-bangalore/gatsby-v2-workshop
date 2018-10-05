f3b3c9af0d618316a6abe6388b1687eade3ee609
jai <jaikant@gmail.com>
2018-10-05 11:06:18 +0530

    initial

---
+++ src/components/layout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default (props) => <div style={{margin: '32px'}}>
+        <h1> My Gatsby Blog </h1>
+        { props.children }
+    </div>
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,28 @@
 import React from 'react'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-export default () => <div> Hello! </div>
 
+export default (props) => 
+    <Layout> 
+        {
+            props.data.allMarkdownRemark.edges.map((trek, index) => {
+                return <h4> { trek.node.frontmatter.title } </h4>
+            })
+        }
+    </Layout>
+
+
+export const query = graphql`
+{
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
